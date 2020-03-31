d04206c94fc139e2c705f2fb54a5fb4680d5c599
Alinur Yaser <alinuer.jasean@gmail.com>
2020-03-31 20:08:04 +0200

    Day_18_DDF DataDrivenFramework, DataProvider

--- src/test/java/com/cybertek/tests/day18_DDF/DDFWithDataProvider.java
+++ src/test/java/com/cybertek/tests/day18_DDF/DDFWithDataProvider.java
@@ -6,32 +6,33 @@ import org.testng.annotations.Test;
 public class DDFWithDataProvider {
 
     @DataProvider
-    public Object[][]testData(){
+    public Object[][] testData() {
 
-        String [][] data= {
-                {"Got","9"},
-                {"HIMYM","9"},
-                {"friends","10"},
-                {"person of Interest","10"},
+        String[][] data = {
+                {"Got", "9"},
+                {"HIMYM", "9"},
+                {"friends", "10"},
+                {"person of Interest", "10"},
                 {"Breaking Bad", "9"},
-                {"Big Bang Theory","9"},
+                {"Big Bang Theory", "9"},
         };
         return data;
     }
 
     // get data from data provider and pass as and argument
     @Test(dataProvider = "testData")
-    public void test1(String tvshow, String rating,String source){
-        System.out.println("Tv show :"+tvshow+" has rating "+rating);
+
+    public void test1(String tvshow, String rating) {
+        System.out.println("Tv show :" + tvshow + " has rating " + rating);
 
     }
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
